949cdfdb120371780c2fc6d5129a13f63299ec31
Mohandkajuluri <md.kajuluri@gmail.com>
2024-01-30 14:45:23 -0500

    pms

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ terraform {
   }
 }
 
-variable "parameters" {
-  default = [
-    { name = "test", type = "string", value = "test" },
-    { name = "test1", type = "string", value = "test1" }
-  ]
-}
-
-resource "aws_ssm_parameter" "params" {
-  count = length(var.parameters)
-  name  = var.parameters[count.index].name
-  type  = var.parameters[count.index].type
-  value = var.parameters[count.index].value
-}
+#variable "parameters" {
+#  default = [
+#    { name = "test", type = "string", value = "test" },
+#    { name = "test1", type = "string", value = "test1" }
+#  ]
+#}
+#
+#resource "aws_ssm_parameter" "params" {
+#  count = length(var.parameters)
+#  name  = var.parameters[count.index].name
+#  type  = var.parameters[count.index].type
+#  value = var.parameters[count.index].value
+#}
