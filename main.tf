ebf5977b66cc4695b8e8fd5d93416b0e4b62af66
Mohandkajuluri <md.kajuluri@gmail.com>
2024-01-31 13:57:06 -0500

    pms

--- main.tf
+++ main.tf
@@ -18,4 +18,5 @@ resource "aws_ssm_parameter" "parameters" {
   name  = var.parameters[count.index].name
   type  = var.parameters[count.index].type
   value = var.parameters[count.index].value
+  overwrite = true
 }
