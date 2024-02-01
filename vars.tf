fdc78a0defacf2dc5a57b9546865d57101ec8171
Mohandkajuluri <md.kajuluri@gmail.com>
2024-01-31 20:39:12 -0500

    rds

--- vars.tf
+++ vars.tf
@@ -3,6 +3,6 @@ variable "parameters" {
   default = [
     { name = "test", type = "String", value = "test" },
     { name = "dev.rds.username", type = "String", value = "admin1" },
-    { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp@123" }
+    { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp123" }
   ]
 }
